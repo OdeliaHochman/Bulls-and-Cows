fa538c5afe137fb1f6e00a6553f01cd87e6f3e36
car <caron1211@gmail.com>
2019-04-22 18:04:43 +0300

    fix test

--- Test.cpp
+++ Test.cpp
@@ -88,66 +88,66 @@ int main() {
 		.CHECK_OUTPUT(calculateBullAndPgia("2093","2093"), "4,0")      // 4 bull, 0 pgia
 		.CHECK_OUTPUT(calculateBullAndPgia("5641","4651"), "2,2")      // 2 bull, 2 pgia
 		.CHECK_OUTPUT(calculateBullAndPgia("000000","000000"), "6,0")      // 6 bull, 0 pgia
-		.CHECK_OUTPUT(calculateBullAndPgia("202020","202002"), "4,2")      // 6 bull, 0 pgia
+		.CHECK_OUTPUT(calculateBullAndPgia("202020","202002"), "4,2")      // 4 bull, 2 pgia
 		
 		//************************NOT-WORK***********************//
-		.CHECK_OUTPUT(calculateBullAndPgia("55","66"), "0,2")      // 0 bull, 2 pgia
-		.CHECK_OUTPUT(calculateBullAndPgia("55","23"), "0,2")      // 0 bull, 2 pgia
-		.CHECK_OUTPUT(calculateBullAndPgia("18","78"), "1,1")      // 1 bull, 1 pgia
-		.CHECK_OUTPUT(calculateBullAndPgia("90","20"), "1,1")      // 1 bull, 1 pgia
-		.CHECK_OUTPUT(calculateBullAndPgia("111","1093"), "1,2")   // 1 bull, 2 pgia--??????
-		.CHECK_OUTPUT(calculateBullAndPgia("777","877"), "2,1")    // 2 bull, 1 pgia
-		.CHECK_OUTPUT(calculateBullAndPgia("548","238"), "1,2")    // 1 bull, 2 pgia
-		.CHECK_OUTPUT(calculateBullAndPgia("3212","54"), "0,4")    // 0 bull, 4 pgia
+		.CHECK_OUTPUT(calculateBullAndPgia("55","66"), "0,0")      // 0 bull, 0 pgia
+		.CHECK_OUTPUT(calculateBullAndPgia("55","23"), "0,0")      // 0 bull, 0 pgia
+		.CHECK_OUTPUT(calculateBullAndPgia("18","78"), "1,0")      // 1 bull, 0 pgia
+		.CHECK_OUTPUT(calculateBullAndPgia("90","20"), "1,0")      // 1 bull, 10 pgia
+		.CHECK_OUTPUT(calculateBullAndPgia("111","1093"), "1,2")   // change to error or something
+		.CHECK_OUTPUT(calculateBullAndPgia("777","877"), "2,0")    // 2 bull, 0 pgia
+		.CHECK_OUTPUT(calculateBullAndPgia("548","238"), "1,0")    // 1 bull, 0 pgia
+		.CHECK_OUTPUT(calculateBullAndPgia("3212","54"), "0,4")    // change to error or something
     //*******************************************************//
 
-			.CHECK_OUTPUT(calculateBullAndPgia("1000000","100000000"), "7,0")      // 7 bull, 0 pgia--????
-		.CHECK_OUTPUT(calculateBullAndPgia("1234567","1030507"), "4,3")      // 4 bull, 3 pgia
+			.CHECK_OUTPUT(calculateBullAndPgia("1000000","100000000"), "7,0")      // change to error or something
+		.CHECK_OUTPUT(calculateBullAndPgia("1234567","1030507"), "4,0")      // 4 bull, 0 pgia
 		.CHECK_OUTPUT(calculateBullAndPgia("6565656565","5656565656"), "0,10")      // 0 bull, 10 pgia--????
 		.CHECK_OUTPUT(calculateBullAndPgia("888888888","888888888"), "9,0")      // 9 bull, 0 pgia
-		.CHECK_OUTPUT(calculateBullAndPgia("4564564564","4564564568"), "9,1")      // 9 bull, 1 pgia
-		.CHECK_OUTPUT(calculateBullAndPgia("3948576","3948501"), "5,2")   // 5 bull, 2 pgia--??????
-		.CHECK_OUTPUT(calculateBullAndPgia("66666666","61616161"), "4,4")    // 4 bull, 4 pgia
-		.CHECK_OUTPUT(calculateBullAndPgia("548","238"), "1,2")    // 1 bull, 2 pgia
-		.CHECK_OUTPUT(calculateBullAndPgia("3212","54"), "0,4")    // 0 bull, 4 pgia
+		.CHECK_OUTPUT(calculateBullAndPgia("4564564564","4564564568"), "9,0")      // 9 bull, 0 pgia
+		.CHECK_OUTPUT(calculateBullAndPgia("3948576","3948501"), "5,0")   // 5 bull, 0 pgia
+		.CHECK_OUTPUT(calculateBullAndPgia("66666666","61616161"), "4,0")    // 4 bull, 0 pgia
+		.CHECK_OUTPUT(calculateBullAndPgia("548","238"), "1,0")    // 1 bull, 0 pgia
+		.CHECK_OUTPUT(calculateBullAndPgia("3212","54"), "0,4")    // change to error or something
 
 		;
 
 		testcase.setname("Play with dummy choosers and guessers")
 		.CHECK_EQUAL(play(a1111, b1111, 4, 100), 1)      // guesser wins in one turn.
-		.CHECK_EQUAL(play(s202020,d202020,6, 10), 2)    // guesser loses by running out of turns 
-		.CHECK_EQUAL(play(e1998,f2039, 4, 5), 0)   // guesser loses technically by making an illegal guess (too long).
-		.CHECK_EQUAL(play(s, d000000, 0, 2), 0)     // chooser loses technically by choosing an illegal number (too long).
+		.CHECK_EQUAL(play(s202020,d202020,6, 10), 1)    //guesser wins in one turn. 
+		.CHECK_EQUAL(play(e1998,f2039, 4, 5), 6)   //  guesser loses by running out of turns.
+		.CHECK_EQUAL(play(s, d000000, 0, 2), 3)     // guesser loses technically by choosing an illegal number (too long).
      
-		 .CHECK_EQUAL(play(e5641, d2222, 4, 100), 1)      // guesser wins in one turn.
-		.CHECK_EQUAL(play(s2, k444, 1, 15), 3)    // guesser loses by running out of turns 
-		.CHECK_EQUAL(play(s872315,b4567, 6, 3), 0)   // guesser loses technically by making an illegal guess (too long).
-		.CHECK_EQUAL(play(a3452, d787878, 4, 12),13)     // chooser loses technically by choosing an illegal number (too long).
+		 .CHECK_EQUAL(play(e5641, d2222, 4, 100), 101)      // guesseloses by rr unning out of turns.
+		.CHECK_EQUAL(play(s2, k444, 1, 15), 16)    // guesser loses by running 
+		.CHECK_EQUAL(play(s872315,b4567, 6, 3), 4)   // guesser loses technically by making an illegal guess (too long).
+		.CHECK_EQUAL(play(a3452, d787878, 4, 12),13)     // guesser loses technically by choosing an illegal number (too long).
 		//**************************************//
 	.CHECK_EQUAL(play(j13567,n13567, 5, 100), 1)      // guesser wins in one turn.
 		.CHECK_EQUAL(play(i700000, d000000, 6, 100), 101)    // guesser loses by running out of turns 
 		.CHECK_EQUAL(play(e5641, m30000, 4, 100), 101)   // guesser loses technically by making an illegal guess (too long).
-		.CHECK_EQUAL(play(a4567, f1998, 4, 100), 0)     // chooser loses technically by choosing an illegal number (too long).
+		.CHECK_EQUAL(play(a4567, f1998, 4, 100), 101)     // chooser loses technically by choosing an illegal number (too long).
 	
 		.CHECK_EQUAL(play(s, d, 0, 100), 1)      // guesser wins in one turn.--??????????
-		.CHECK_EQUAL(play(h201, n201, 3, 100), 101)    // guesser loses by running out of turns 
+		.CHECK_EQUAL(play(h201, n201, 3, 100), 0)    // 
 		.CHECK_EQUAL(play(i3498, x56, 4, 100), 101)   // guesser loses technically by making an illegal guess (too long).
-		.CHECK_EQUAL(play(j40404040, k666, 8, 100), 0)     // chooser loses technically by choosing an illegal number (too long).
+		.CHECK_EQUAL(play(j40404040, k666, 3, 100), 0)     // chooser loses technically by choosing an illegal number (too long).
 	
-		.CHECK_EQUAL(play(s0909090, m30000, 7, 100), 1)      // guesser wins in one turn.
+		.CHECK_EQUAL(play(s0909090, m30000, 7, 100), 1)      // lose tec
 		.CHECK_EQUAL(play(p23, x60, 2, 100), 101)    // guesser loses by running out of turns 
 		.CHECK_EQUAL(play(e5641, n5, 4, 9), 10)   // guesser loses technically by making an illegal guess (too long).
-		.CHECK_EQUAL(play(j890098, k765, 6, 16), 0)     // chooser loses technically by choosing an illegal number (too long).
+		.CHECK_EQUAL(play(j890098, k765, 3, 16), 0)     // chooser loses technically by choosing an illegal number (too long).
 	//****************************
-    .CHECK_EQUAL(play(p00, g1234, 2, 1), 2)      // guesser wins in one turn.
-		.CHECK_EQUAL(play(h222, k333, 3, 1), 1)    // guesser loses by running out of turns 
+    .CHECK_EQUAL(play(p00, g1234, 2, 1), 2)      // 
+		.CHECK_EQUAL(play(h222, k333, 3, 1), 2)    // guesser loses by running out of turns 
 		.CHECK_EQUAL(play(p00, g12345, 2, 100), 101)   // guesser loses technically by making an illegal guess (too long).
-		.CHECK_EQUAL(play(p54, x60, 2, 3), 0)     // chooser loses technically by choosing an illegal number (too long).
+		.CHECK_EQUAL(play(p54, x60, 2, 3), 4)     // chooser loses technically by choosing an illegal number (too long).
 	
 		.CHECK_EQUAL(play(s2, d2, 1, 3), 1)      // guesser wins in one turn.
 		.CHECK_EQUAL(play(p87,x77, 2, 100), 101)    // guesser loses by running out of turns 
 		.CHECK_EQUAL(play(a4567,k765, 4, 8), 9)   // guesser loses technically by making an illegal guess (too long).
-		.CHECK_EQUAL(play(e2039,g1234, 4, 100), 0)     // chooser loses technically by choosing an illegal number (too long).
+		.CHECK_EQUAL(play(e2039,g1234, 4, 100), 101)     // chooser loses technically by choosing an illegal number (too long).
 	
 	;
 	testcase.setname("Play with smart guesser");
