ae5629b512bc162f4febc5784bdf5f52d3273f60
Efrat Cohen <cohenefratoni747@gmail.com>
2019-04-30 13:46:08 +0300

    add calculate

--- calculate.cpp
+++ calculate.cpp
@@ -1,13 +1,58 @@
 #include "calculate.hpp"
-
+using std::string;
+#include<string>
+#include <sstream>      // std::stringstream
+using namespace std;
 
 namespace bullpgia 
 {
-
+    
  string calculateBullAndPgia(string choice, string guess)
  {
-     string str="";
-     return str;
+     int bull=0;
+     int pgia=0;
+     string checkChoice=choice;
+     string checkGuess=guess;
+
+     if(choice.length!=guess.length)
+      {
+         throw("Error!The lengths are not equal");
+      }
+      else
+         for(int i=0; i<choice.length; i++){
+           for(int j=0; j<choice.length; j++)
+         {
+             if(i==j && choice[i]==guess[j])
+             {
+                checkChoice[i]='T';
+                checkGuess[i]='T';
+                bull++;
+             }
+             
+         }
+     }
+    for(int i=0; i<checkChoice.length; i++)
+    {
+           for(int j=0; j<checkGuess.length; j++)
+         {
+             if(checkChoice[i]!='T' && checkGuess[j]!='T' && checkChoice[i]!=checkGuess[j] && i!=j)
+             {
+                checkChoice[i]='T';
+                 checkGuess[j]='T';
+                 pgia++;
+                
+             }
+     }   
  }
 
+    // std::stringstream bullPgia;
+    //  bullPgia>>bull>>pgia;
+    //  string ans=bullPgia;
+    //         return bullPgia;
+        std::string bullPgia = std::to_string(bull)+std::to_string(pgia);
+
+        return bullPgia;
+
+   }
+     
 };
